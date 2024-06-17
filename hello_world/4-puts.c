a21efbb5d81472973429871efc361a03c1434f95
evrade10 <safouevrade@gmail.com>
2024-06-17 14:39:05 +0200

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/*
+ * Main function of the program
+ * Returns 0 to indicate that the program terminated successfully
+ */
 int main(void)
 {
-	puts("Programming is like building a multiline puzzle");
-	return 0;
+    /* Using the puts function to display the message */
+    puts("Programmer, c'est comme construire un puzzle multilingue");
+    
+    /* Returns 0 to indicate that the program terminated successfully */
+    return 0;
 }
