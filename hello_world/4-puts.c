491b68de95d1fc129457b552bc56baccff8e2dd4
evrade10 <safouevrade@gmail.com>
2024-06-17 15:23:50 +0200

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
 /*
- * Main function of the program
- * Returns 0 to indicate that the program terminated successfully
- */
-int main(void)
-{
-	puts("Programming is like building a multilingual puzzle);
-
-	return 0;
-}
+ *Main function of the program 
+ *returns 0 to indicate that the program terminated successfully
+ int main(void)
+ {
+ puts("/"Programming is like building a multilingual puzzle");
+ return 0;
+ } 
