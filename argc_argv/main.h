8c3b30013adf954b9bbf8bcee510b4d8958e0ce8
evrade10 <safouevrade@gmail.com>
2024-07-03 23:27:11 +0000

    Write a program that prints its name, followed by a new line

---
+++ argc_argv/main.h
@@ -0,0 +1,13 @@
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int _atoi(char *s);
+int i, d, n, len, f, digit;
+int main(int argc, char *argv[]);
+int result, num1, num2;
+int check_num(char *str);
+unsigned int count;
+int main(int argc, char *argv[]);
+int str_to_int;
+int sum = 0;
+ 
